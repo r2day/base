39120601bd4d08e853e8b7b8e9bd7ea7551cfacb
reday <redayxu@tencent.com>
2022-07-31 23:40:24 +0800

    add id & status

---
+++ util/id.go
@@ -0,0 +1,48 @@
+package common
+
+import (
+	"fmt"
+	"github.com/bwmarrin/snowflake"
+)
+
+func getId(prefix string) string {
+	node, err := snowflake.NewNode(1)
+	if err != nil {
+		fmt.Println(err)
+		return "-"
+	}
+
+	// Generate a snowflake ID.
+	id := node.Generate()
+	return fmt.Sprintf("%s%d", prefix, id)
+}
+
+// GetBrandId 获取品牌id
+func GetBrandId() string {
+	return getId("b")
+}
+
+// GetCategoryId 获取分组
+func GetCategoryId() string {
+	return getId("c")
+}
+
+// GetDishesId 获取菜品id
+func GetDishesId() string {
+	return getId("d")
+}
+
+// GetDepartmentId 部门id
+func GetDepartmentId() string {
+	return getId("dp")
+}
+
+// GetUnitId 规格id
+func GetUnitId() string {
+	return getId("u")
+}
+
+// GetAccountId 账号
+func GetAccountId() string {
+	return getId("AC")
+}
---
+++ util/status.go
@@ -0,0 +1,23 @@
+package common
+
+const (
+	unknownStatus = "unknown"
+)
+
+var (
+	statusMap = map[string]string{
+		"0": "offline",
+		"1": "effected",
+		"2": "pending",
+		"3": "success",
+		"4": "failed",
+	}
+)
+
+// GetStatus filter thr status from client input
+func GetStatus(s string) string {
+	if val, ok := statusMap[s]; ok {
+		return val
+	}
+	return unknownStatus
+}

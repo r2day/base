b32c84bac3c7ac1344aa1de259d781bd8592b27d
reday <redayxu@tencent.com>
2022-07-31 23:15:10 +0800

    add base

---
+++ middle/init.go
@@ -0,0 +1,21 @@
+package middle
+
+import (
+	"bytes"
+	"github.com/gin-gonic/gin"
+)
+
+type CustomResponseWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (w CustomResponseWriter) Write(b []byte) (int, error) {
+	w.body.Write(b)
+	return w.ResponseWriter.Write(b)
+}
+
+func (w CustomResponseWriter) WriteString(s string) (int, error) {
+	w.body.WriteString(s)
+	return w.ResponseWriter.WriteString(s)
+}

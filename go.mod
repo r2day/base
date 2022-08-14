070b919e30cc10e072b22c797974356afb62b5d8
reday <redayxu@tencent.com>
2022-08-14 16:54:37 +0800

    add amqp

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gin-gonic/gin v1.8.1
 	github.com/joho/godotenv v1.4.0
 	github.com/pborman/uuid v1.2.1
+	github.com/rabbitmq/amqp091-go v1.4.0
 	github.com/sirupsen/logrus v1.9.0
 	github.com/spf13/viper v1.12.0
 )
---
+++ middle/amqp.go
@@ -0,0 +1,88 @@
+package middle
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+
+	logger "github.com/r2day/base/log"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		logger.Logger.Panicf("%s: %s", msg, err)
+	}
+}
+
+type Amqp struct {
+	Conn    *amqp.Connection
+	Channel *amqp.Channel
+	Queue   amqp.Queue
+}
+
+func InitAMQP(address string) Amqp {
+
+	var instance Amqp
+	conn, err := amqp.Dial(address)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	instance.Conn = conn
+	instance.Channel = ch
+	instance.Queue = q
+
+	return instance
+
+}
+
+func (i Amqp)Send(payload []byte) {
+
+	body := "Hello World!"
+	err := i.Channel.Publish(
+		"",     // exchange
+		i.Queue.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        payload,
+		})
+	failOnError(err, "Failed to publish a message")
+	logger.Logger.Infof(" [x] Sent %s\n", body)
+}
+
+func (i Amqp) Reiceive(f func([]byte) []byte) {
+	msgs, err := i.Channel.Consume(
+		i.Queue.Name, // queue
+		"",           // consumer
+		true,         // auto-ack
+		false,        // exclusive
+		false,        // no-local
+		false,        // no-wait
+		nil,          // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			logger.Logger.Infof("Received a message: %s", d.Body)
+			f(d.Body)
+		}
+	}()
+
+	logger.Logger.Warn(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}

0fa3760033328ff27388d5db769a9b183648e5b4
reday <redayxu@tencent.com>
2022-11-10 12:58:21 +0800

    add parser

---
+++ parser/url.go
@@ -0,0 +1,80 @@
+package parser
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+const (
+	defaultOffset = 0
+	defaultLimit  = 24
+)
+
+// QueryRequest Binding from JSON
+type QueryRequest struct {
+	// FilterRequest 过滤器
+	Filter []FilterRequest
+}
+
+// FilterRequest Binding from JSON
+type FilterRequest struct {
+	// id 唯一帐户id
+	Id []uint `form:"id" json:"id" xml:"id"`
+
+	// Status 状态
+	Status string `form:"status" json:"status" xml:"status"`
+}
+
+// UrlParams 将url的参数统一进行解析
+type UrlParams struct {
+	Limit     int  `form:"limit" json:"limit" xml:"limit"`
+	Offset    int  `form:"offset" json:"offset" xml:"offset"`
+	HasFilter bool `form:"has_filter" json:"has_filter" xml:"has_filter"`
+	Filter    FilterRequest
+}
+
+func ParserParams(c *gin.Context) UrlParams {
+	params := UrlParams{}
+	filter, ok := c.GetQueryArray("filter")
+	rangeValue, ok := c.GetQueryArray("range")
+
+	params.Limit = defaultLimit
+	params.Offset = defaultOffset
+
+	if len(rangeValue) == 1 {
+		println("rangeValue-->", rangeValue[0], ok)
+		rangeObj := make([]int, 2)
+		err := json.Unmarshal([]byte(rangeValue[0]), &rangeObj)
+
+		if err != nil {
+			fmt.Println("json.Unmarshal failed-->", err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return params
+		}
+		params.Offset = rangeObj[0]
+
+		if rangeObj[1] > 0 {
+			params.Limit = rangeObj[1]
+		}
+	}
+
+	if ok && len(filter) != 0 {
+
+		filterInstance := FilterRequest{}
+
+		err := json.Unmarshal([]byte(filter[0]), &filterInstance)
+		if err != nil {
+			// 如果是空的会解析失败
+			return params
+		}
+
+		if filterInstance.Status == "" {
+			return params
+		}
+		params.HasFilter = true
+		params.Filter = filterInstance
+	}
+	return params
+}
